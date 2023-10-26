f438a6058ef9f624f2c2df46bf7acbe4a625ac4b
SebastianWirstrom <sebastianwirstrom@gmail.com>
2023-10-26 13:43:13 +0200

    PartnersLogo completed 20231026

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Header/Header'
-import Showcase from './components/Showcase/Showcase';
+import PartnersLogo from './components/PartnersLogo/PartnersLogo';
+import Showcase from './components/Showcase/Showcase'
+
 
 function App() {
   return (
     <>
       <Header />
       <Showcase />
+      <PartnersLogo />
+      
     </>
   );
 }
---
+++ src/components/PartnersLogo/PartnersLogo.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import img_partnersLogo_1 from '../../assets/images/Paperz.svg'
+import img_partnersLogo_2 from '../../assets/images/Dorfus.svg'
+import img_partnersLogo_3 from '../../assets/images/Martino.svg'
+import img_partnersLogo_4 from '../../assets/images/Square.svg'
+import img_partnersLogo_5 from '../../assets/images/Gobona.svg'
+
+function PartnersLogo() {
+  return (
+    
+    <section className="partners-logo">
+        <div className="container">
+            <img src={img_partnersLogo_1} alt="Partner logo"/>
+            <img src={img_partnersLogo_2} alt="Partner logo"/>
+            <img src={img_partnersLogo_3} alt="Partner logo"/>
+            <img src={img_partnersLogo_4} alt="Partner logo"/>
+            <img src={img_partnersLogo_5} alt="Partner logo"/>
+        </div>
+    </section>
+    
+  )
+}
+
+export default PartnersLogo
