813e963acd6b1a457e12995176783402d3a4b930
SebastianWirstrom <sebastianwirstrom@gmail.com>
2023-11-09 16:09:36 +0100

    completed forms 20231109

--- src/components/ContactSection/ContactForm.js
+++ src/components/ContactSection/ContactForm.js
@@ -1,25 +1,69 @@
-import React from 'react'
-import Button from '../Generics/Button/Button'
+import React, { useState } from 'react'
+
 
 const ContactForm = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+  const [successMessage, setSuccessMessage] = useState('')
+  const [showError, setShowError] = useState(false)
+  const [showSuccess, setShowSuccess] = useState(false)
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const userMessage = { name, email, message }
+    const json = JSON.stringify(userMessage)
+
+    const result = await fetch('https://win23-assignment.azurewebsites.net/api/contactform', {
+      method: 'post',
+      headers: { 'content-type': 'application/json'},
+      body: json
+    })
+
+    if (result.status === 200) {
+      clearForm()
+      setShowSuccess(true)
+      setShowError(false)
+      setSuccessMessage('Your message has been submitted!')
+      setErrorMessage('')
+    }
+    else {
+      setShowError(true)
+      setShowSuccess(false)
+      setErrorMessage('Something went wrong, try again!')
+      setSuccessMessage('')
+    }
+  }
+
+  const clearForm = () => {
+    setName('')
+    setEmail('')
+    setMessage('')
+    setShowError(false)
+  }
   
   return (
     <>
-      <form id="contact-form" method="post">
+      <form id="contact-form" onSubmit={handleSubmit} noValidate>
           <div className="form-row">
-              <label className="form-label" htmlFor="name"></label>
-              <input className="form-input" type="text" id="name" name="name" title="name" placeholder="Name*" tabIndex="1"></input>
+            <label className="form-label"/>
+            <input className="form-input" value={name} placeholder="Name*" tabIndex="1" onChange={(e) => setName(e.target.value)}/>
           </div>
           <div className="form-row">
-              <label className="form-label" htmlFor="email"></label>
-              <input className="form-input" type="email" id="email" name="email" title="Epostadress" placeholder="Email*" tabIndex="2"></input>
+            <label className="form-label"/>
+            <input className="form-input" value={email} placeholder="Email*" tabIndex="2" onChange={(e) => setEmail(e.target.value)}/>
           </div>
           <div className="form-row">
-                  <label className="form-label" htmlFor="biography"></label>
-                  <textarea className="form-input" id="biography" name="biography" placeholder="Your Message*" tabIndex="3"></textarea> 
+            <label className="form-label"/>
+            <textarea className="form-input" value={message} placeholder="Your Message*" tabIndex="3" onChange={(e) => setMessage(e.target.value)}/>
           </div>
-          <Button text="Send Message"  icon="fa-regular fa-arrow-up-right"/>
+          <button type="submit" text="Send Message" className="btn-submit">Send Message<i className="fa-regular fa-arrow-up-right"></i></button>
+          <div className="submit-message">
+            {showError && <div className="error-message">{errorMessage}</div>}
+            {showSuccess && <div className="success-message">{successMessage}</div>}
+          </div>
       </form>
     </>
   )
